0c3b0a4ab897c3d537eef8744d1c3c48ef843641
Jakub Zíka <info@jakubzika.dev>
2023-02-23 22:02:42 +0100

    Added animation created by ChatGPT.

--- app/src/main/java/dev/jakubzika/jetpack_compose_animations/MainActivity.kt
+++ app/src/main/java/dev/jakubzika/jetpack_compose_animations/MainActivity.kt
@@ -5,60 +5,22 @@ package dev.jakubzika.jetpack_compose_animations
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.AnimatedContent
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.Crossfade
-import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.animation.animateContentSize
-import androidx.compose.animation.core.LinearEasing
-import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.animateFloat
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.infiniteRepeatable
-import androidx.compose.animation.core.rememberInfiniteTransition
-import androidx.compose.animation.core.spring
-import androidx.compose.animation.core.tween
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
-import androidx.compose.animation.slideInHorizontally
-import androidx.compose.animation.slideOutHorizontally
-import androidx.compose.animation.with
+import androidx.compose.animation.*
+import androidx.compose.animation.core.*
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.LinearProgressIndicator
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowForward
-import androidx.compose.material.icons.filled.CheckCircle
-import androidx.compose.material.icons.filled.KeyboardArrowRight
-import androidx.compose.material.icons.filled.Refresh
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.material.icons.filled.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import dev.jakubzika.jetpack_compose_animations.ui.theme.JetpackcomposeanimationsTheme
 import kotlinx.coroutines.Dispatchers
@@ -114,6 +76,11 @@ fun ContentView() {
         AnimatedDropdownExample(data)
         contentSpace()
         AnimatedCarouselExample(data)
+        contentSpace()
+        contentSpace()
+        contentSpace()
+        contentSpace()
+        ChatGPT_SpiralAnimation()
     }
 }
 
@@ -301,6 +268,51 @@ private fun AnimatedCarouselExample(data: Array<Pair<String, String>>) {
             Text(text = currentItem.first, style = MaterialTheme.typography.h6)
             Text(text = currentItem.second, style = MaterialTheme.typography.h6)
         }
+    }
+}
+
+@Composable
+fun ChatGPT_SpiralAnimation() {
+    var rotationAngle by remember { mutableStateOf(0f) }
+    val spiralAnimation = rememberInfiniteTransition()
+    val spiralAngle by spiralAnimation.animateFloat(
+        initialValue = 0f,
+        targetValue = 720f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(2000, easing = LinearEasing),
+            repeatMode = RepeatMode.Restart
+        )
+    )
+
+    val spiralRadius by spiralAnimation.animateFloat(
+        initialValue = 0f,
+        targetValue = 200f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(2000, easing = LinearEasing),
+            repeatMode = RepeatMode.Restart
+        )
+    )
+
+    rotationAngle = spiralAngle
+
+    Box(
+        modifier = Modifier
+            .size(64.dp)
+            .background(color = MaterialTheme.colors.background)
+    ) {
+        Icon(
+            imageVector = Icons.Default.Favorite,
+            contentDescription = null,
+            modifier = Modifier
+                .offset {
+                    val x =
+                        (spiralRadius * kotlin.math.cos(Math.toRadians(spiralAngle.toDouble()))).toFloat()
+                    val y =
+                        (spiralRadius * kotlin.math.sin(Math.toRadians(spiralAngle.toDouble()))).toFloat()
+                    return@offset IntOffset(x.toInt(), y.toInt())
+                }
+                .rotate(rotationAngle),
+        )
     }
 }
 
